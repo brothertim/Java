089e59ebd77e2af1ae40564c56e04b3307b0a07c
brothertim <timothyj.smith8@gmail.com>
2017-11-28 10:46:45 -0700

    Update Project.java

--- Project.java
+++ Project.java
@@ -17,7 +17,7 @@ public class Project
 {
 	public static void main(String[] args)
 	{
-		//try&catch block to validate input file and catch exceptions, exit program if exceptions occur
+		
 		String inputString = "";
 		String uniqueList = "";
 		String ascendingWordList = "";
@@ -26,7 +26,7 @@ public class Project
 		int wordCount; 
 		int uniqueCount;
 		
-		try 
+		try  //try&catch block to validate input file and catch exceptions, exit program if exceptions occur
 		{
 			inputString = new String(Files.readAllBytes(Paths.get(args[0])));
 			//System.out.print(s1);
@@ -43,9 +43,6 @@ public class Project
 		wordCount = wordCounter(inputString);
 		uniqueCount = uniqueWordCounter(inputString);
 		ascendingWordList = ascendingList(inputString);
-		
-/*ask*/	//String ascendingList = String.join(", ", ascendingWordList); //convert a list to a string, comma separated
-		// int uniqueWordCount = uniqueList.size(); //get size of hashset and assign to uniqueWordCount for later printing
 		
 		
 		
@@ -98,7 +95,7 @@ public class Project
 		return ascendingWordList;
 	}
 		
-		
+	//this section for more methods	
 		
 		
 		
@@ -107,3 +104,43 @@ public class Project
 		
 		
 }
+
+
+
+/* this is an example paragraph to process:
+
+When I first brought my cat home from the humane society she was a mangy, 
+pitiful animal. It cost a lot to adopt her: forty dollars. And then I had 
+to buy litter, a litterbox, food, and dishes for her to eat out of. Two 
+days after she came home with me she got taken to the pound by the animal
+warden. There's a leash law for cats in Fort Collins. If they're not in your 
+yard they have to be on a leash. Anyway, my cat is my best friend. I'm glad 
+I got her. She sleeps under the covers with me when it's cold. Sometimes she 
+meows a lot in the middle of the night and wakes me up, though. 
+*/
+
+/* this is the program output based on the above paragraph example
+
+E:\java Project example.txt
+
+The total number of words in the file is: 122
+The total number of different words in the file is: 85
+Words of the input file in ascending order without duplication
+And Anyway Collins Fort I I'm If It She Sometimes There's Two When a adopt after and animal be best 
+brought buy by came cat cats cold cost covers days dishes dollars eat first food for forty friend 
+from glad got had have her home humane in is it's law leash litter litterbox lot mangy me meows middle
+my night not of on out pitiful pound she sleeps society taken the then they they're though to under 
+up wakes warden was when with yard your
+
+E:\
+
+*/
+
+// to do:  add a prompt to allow user to input a word to search for.  EINPUT is command to terminate program
+// ex:  Please enter a search pattern:  the <enter>  (in this case, user searches for the word "the")
+// ex:  Line number:  1  (print which line number that word appears on)
+// ex:  When I first brought my cat home from the humane society she was a mangy,  (print the contents of the line)
+// ex:                                        ^   (print an arrow under the first letter of the word)
+// ex:  Line number: 4  (since it appears on more than one line)
+// ex:  days after she came home with me she got taken to the pound by the animal
+// ex:                          			  ^
