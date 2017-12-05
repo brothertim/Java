5b855a32917f3e54392529803aa885c83070daa2
brothertim <timothyj.smith8@gmail.com>
2017-12-04 18:05:18 -0700

    Add files via upload
    if you compile and execute it, you need to either pass a txt file via command line, or have a file named example.txt, in the directory.

--- Project.java
+++ Project.java
@@ -26,15 +26,23 @@ public class Project
 	public static void main(String[] args)
 	{
 		
+		String filename = "example.txt"; //placeholder
+		if (args.length > 0)
+		{
+			filename = args[0];
+		} else
+		{
+			System.out.println("no filename specified, using default 'example.txt' file");
+		}
 		//method calls for total word count
 		String totalArray[] = new String[5000];  //wanted to use a static array because it's harder to manipulate and I wanted the practice, I use ArrayLists later on.
-		totalArray = totalWord(totalArray);  //method call to create array
+		totalArray = totalWord(totalArray, filename);  //method call to create array
 		int wordCount; 
 		wordCount = counter(totalArray);  //calling method to count array elements
 		
 		//method calls for unique word count
 		String uniqueArray[] = new String[wordCount];  //make new array of a minimal size
-		uniqueArray = filterDuplicates(wordCount);  //method call to filter out duplicates
+		uniqueArray = filterDuplicates(wordCount, filename);  //method call to filter out duplicates
 		int uniqueCount;
 		uniqueCount = counter(uniqueArray);  //calling counter method again
 		
@@ -63,47 +71,33 @@ public class Project
 			}
 
 			List<String> lineList = new ArrayList<>();
-			lineList = createLineList();  //create a list with each element being an entire line from input document
-			//DEBUG// 
-			//System.out.print(lineList); //^ is working
+			lineList = createLineList(filename);  //create a list from input document, line by line
+			//System.out.print(lineList);  //print list to verify it's being stored properly
 			
-			//test
-			//String[] lines = new String[lineList.size()];
-			//lines = lineList.toArray(lines);
-
-//TODO:	fix this part, always outputs "Line Number 1" and "Timothy Smith", even when first called.
 			for(String s : lineList) //iterate through list
 			{
-				if (s.contains(searchString) != false)  //if element contains a match
+				if (s.contains(searchString)) //do this if element contains searchString within
 				{
-					System.out.printf("Line number " + (lineList.indexOf(searchString)+2));  //print the index value +1
+					System.out.printf("%nLine number " + (lineList.indexOf(s)+1));  //print the index value +1
 					System.out.println();
-					System.out.printf(lineList.get(lineList.indexOf(searchString)+1));
-					System.out.println();
-					//put code for arrow here
-					System.out.println("get index of arrow here");
-					System.out.println();
-					//System.out.println();
-					
-				}
-			
-			}	
-			
-			
+					System.out.println(s); //print current element
 
-			//search returned element for index of matched word, use index as spacing for arrow below
-		//	String arrow = new String;
-		//	arrow = ("%d");
-		//	arrow += space; //append the space after the %d
-		//	arrow += "s";  //append s to end of string, to end up with %ds, where d=index where arrow will be, i.e. %15s
-		//	System.out.printf("%s", arrow )  //create an arrow at the index matching the first letter of the matched results.
-			
-		
-			
+					//arrow printing
+					String arrow = "^";
+					String space = "";
+					for ( int i = -1; (i = s.indexOf(searchString, i)) != -1; i++)  //iterate through the string to find word(s), if word is found (!= -1), do rest
+					{
+						space = String.format("%" + i + "s", " ");  //create a string that is %Xs where X is variable integer
+						System.out.printf("%s%s", space, arrow);  //exception thrown when index is zero (first word)   TODO: FIX THIS, also, spacing is off on second result in line.
+					} 
+				} //end if statement
+			} //end for loop		
 			
 		} while (!searchString.equals(terminateString));  //loop until terminate value is matched to input
 	} //end main
 
+	
+	/*----------methods-----------*/
 	
 	//reusable method to count array elements with data
 	public static int counter(String[] array)
@@ -116,15 +110,13 @@ public class Project
 		
 	}
 	
-	
-	
 	// read text file, delimit, place values into array
-	public static String[] totalWord(String[] array)
+	public static String[] totalWord(String[] array, String path)
 	{
 		Scanner scan = null;
 		try 
 		{
-			scan = new Scanner(new File("example.txt")).useDelimiter("[^a-zA-Z0-9']+");  //delimiting non-alphanumeric, but exempted apostrophes also in case of contractions
+			scan = new Scanner(new File(path)).useDelimiter("[^a-zA-Z0-9']+");  //delimiting non-alphanumeric, but exempted apostrophes also in case of contractions
 		} catch (FileNotFoundException e)   //if file not found, exit
 		{
 			System.out.println(e);
@@ -141,15 +133,13 @@ public class Project
 		return array; //return the array
 	}
 	
-	
-	
 	//method to filter unique words from array
-	public static String[] filterDuplicates(int wordCount)
+	public static String[] filterDuplicates(int wordCount, String path)
 	{
 		Scanner scan = null;  //rescan document//  TO DO:  place another method call here and at line 102-110 to do this and just reuse that instead
 		try 
 		{
-			scan = new Scanner(new File("example.txt")).useDelimiter("[^a-zA-Z0-9']+");
+			scan = new Scanner(new File(path)).useDelimiter("[^a-zA-Z0-9']+");
 		} catch (FileNotFoundException e) 
 		{
 			System.out.println(e);
@@ -180,9 +170,6 @@ public class Project
 		return filteredArray;	//return the filtered array
 	}
 	
-	
-	
-	
 	//method for printing ascending unique words
 	public static String printUniques(String[] uniqueArray)
 	{
@@ -201,7 +188,7 @@ public class Project
 		
 		Collections.sort(stringList);
 		
-		/*
+		/*  use this if fix sorter
 		//get sorted array
 		String[] sortedArray = new String[uniqueArray.length];
 		sortedArray = sorter(uniqueArray, 1); 
@@ -219,8 +206,7 @@ public class Project
 		return uniqueString;
 	}
 	
-	
-	//method to manually sort, nullointerexception being thrown
+	//method to manually sort, nullointerexception being thrown, fix if there's time
 /*	private static String[] sorter(String[] array, int position)
 	{
 		String[] aux = new String[array.length];
@@ -248,22 +234,20 @@ public class Project
 		return aux;
   }  */
   
-  
-	
-	//method to convert input file into a list of whole lines from doc, using an ArrayList because I'd rather not use a fixed size list again for an unknown array size
-	public static List<String> createLineList()
+	//method to convert input file into a list of whole lines from doc
+	public static List<String> createLineList(String path)
 	{
-		Scanner scan = null;  //TODO:  same as line 127,  this could clean up the code more and put one single place to change "example.txt" to whatever the file will be called
+		Scanner scan = null;
 		try 
 		{
-			scan = new Scanner(new File("example.txt")).useDelimiter("[^a-zA-Z0-9']+");
+			scan = new Scanner(new File(path));
 		} catch (FileNotFoundException e) 
 		{
 			System.out.println(e);
 			System.exit(1);
 		} 		
 		
-		List<String> list = new ArrayList<String>();  //create new list in which to store inputs
+		List<String> list = new ArrayList<String>();  //create new list in which to store inputs, using list instead of static array this time
 		String temp = new String(); 
 		while (scan.hasNextLine())  //while more data to be read on next line
 		{ 
@@ -272,7 +256,6 @@ public class Project
 		}
 		scan.close();
 		return list;
-		//use linear search CH19 powerpoint to find array element matching search string
 	}
 	
-}
+} //end class
